6fcd726968492192b693f0aac5a46b4c22eff6cb
Mohammad Ali Karimi <ali216k@gmail.com>
2023-05-06 14:56:26 +0000

    built out sign-in/sign-out RCAVs using Devise gem

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,13 +1,6 @@
 class ApplicationController < ActionController::Base
   
-  before_action :force_user_sign_in
+  before_action :authenticate_user!
 
-  def force_user_sign_in
-
-    if session[:user_id].present?
-      redirect_to "/users/sign_in"
-    end
-    
-  end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
+  devise_for :users
+  resources :actors
   root "movies#index"
   
-  resources :actors
   resources :movies
 end
