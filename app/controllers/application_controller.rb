a4e518021cc50654669b9242cceaa67ddfda3e4c
Mohammad Ali Karimi <ali216k@gmail.com>
2023-05-06 13:35:48 +0000

    Made card smaller, use before_action for user sign in

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,13 @@
 class ApplicationController < ActionController::Base
+  
+  before_action :force_user_sign_in
+
+  def force_user_sign_in
+
+    if session[:user_id].present?
+      redirect_to "/users/sign_in"
+    end
+    
+  end
+
 end
